868c79c5e9ef8852e0193714639606fcee73db0d
Tiansi Dong <tdong@uni-bonn.de>
2016-05-15 22:57:19 +0200

    this version extends OS into OBEU Quick start procedure is described in docker-config/README Users can choose OBEU data type for each CSV column Users can download datapackage2json

---
+++ repos/os-packager/app/front/scripts/config/env.js
@@ -0,0 +1,30 @@
+;(function(angular) {
+
+  var services = require('app/services');
+
+  var config = {
+    defaultErrorHandler: function(error) {
+      if (console.trace) {
+        return console.trace(error);
+      } else
+      if (console.log) {
+        return console.log(error);
+      }
+    },
+    defaultPackageFileName: 'datapackage2.json', //add by TD;
+    events: {
+      CONCEPTS_CHANGED: 'package.conceptsChanged'
+    },
+    storage: {
+      collection: 'appstate',
+      key: 'default'
+    },
+    steps: services.data.steps,
+    isWizard: window.isWizard,
+    maxFileSizeToStore: 100 * 1024 * 1024 // 100Mb
+  };
+
+  angular.module('Application')
+    .constant('Configuration', config);
+
+})(angular);
